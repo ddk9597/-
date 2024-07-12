f1fe11ae9bcda0965fc7c821937f212a77537924
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-07-12 22:43:34 +0900

    초기셋팅

---
+++ src/main/java/com/kch/study/JustDoitApplication.java
@@ -0,0 +1,13 @@
+package com.kch.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustDoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustDoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kch/study/JustDoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kch.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustDoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
