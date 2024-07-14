ed48930b5ecda05ad6c6936e1f6bf72df18a39b2
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-07-14 22:48:06 +0900

    no message

--- src/main/java/com/kch/study/JustDoitApplication.java
+++ src/main/java/com/kch/study/JustDoitApplication.java
@@ -2,8 +2,10 @@ package com.kch.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// 스프링시큐리티 비활성화 -> 사이트 이용 자유롭게 가능함
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class JustDoitApplication {
 
 	public static void main(String[] args) {
