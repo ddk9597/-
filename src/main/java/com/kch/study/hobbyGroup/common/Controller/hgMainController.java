a87a43dc27bc1eb65c907b4f8ab272bef130b2ef
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-07-18 23:50:37 +0900

    no message

--- src/main/java/com/kch/study/hobbyGroup/common/Controller/hgMainController.java
+++ src/main/java/com/kch/study/hobbyGroup/common/Controller/hgMainController.java
@@ -1,5 +1,19 @@
 package com.kch.study.hobbyGroup.common.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kch.study.hobbyGroup.common.model.service.hgService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("hg")
+@RequiredArgsConstructor
 public class hgMainController {
 
+	private final hgService service;
+	
 }
