77f079ebaa9fee80323b92542d7f4b6ce214a4eb
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-07-20 22:51:54 +0900

    패키지 정리

--- src/main/java/com/kch/study/hobbyGroup/common/model/service/hgService.java
+++
@@ -1,5 +0,0 @@
-package com.kch.study.hobbyGroup.common.model.service;
-
-public interface hgService {
-
-}
--- src/main/java/com/kch/study/hobbyGroup/common/model/service/hgServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.kch.study.hobbyGroup.common.model.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class hgServiceImpl implements hgService {
-
-}
