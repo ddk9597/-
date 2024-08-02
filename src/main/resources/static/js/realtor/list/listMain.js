20b6c2721be91d67fb1d29c27cf1d04c59061868
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-08-03 07:07:46 +0900

    realtor > 모달, 푸터 다듬음

--- src/main/resources/static/js/realtor/list/listMain.js
+++ src/main/resources/static/js/realtor/list/listMain.js
@@ -1,9 +1,17 @@
+// 모든 모달 닫기
+function closeAllModals() {
+  const modals = document.getElementsByClassName('modal');
+  for (let i = 0; i < modals.length; i++) {
+      modals[i].style.display = 'none';
+  }
+}
+
 // 모달 열기
 function openModal(modalId, triggerElement) {
+  closeAllModals();  // 다른 모든 모달을 닫음
   const modal = document.getElementById(modalId);
   modal.style.display = 'block';
   
-  // 모달을 트리거 요소 아래에 위치시킵니다.
   const rect = triggerElement.getBoundingClientRect();
   modal.style.top = rect.bottom + 'px';
   modal.style.left = rect.left + 'px';
