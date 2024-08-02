0764b68753e5384f7067e84270c88cab14074920
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-08-03 03:37:02 +0900

    rMain > 공통 헤더 설정

--- src/main/java/com/kch/study/realtor/controller/rMainController.java
+++ src/main/java/com/kch/study/realtor/controller/rMainController.java
@@ -23,40 +23,40 @@ import lombok.extern.slf4j.Slf4j;
 public class rMainController {
 
 	private final rServiceImpl service;
-	
+
 	// 메인 페이지로 이동하기
-    @GetMapping
-    public String mainPage(Model model, HttpSession session) {
-        rMember loginMember = (rMember) session.getAttribute("loginMember");
-        model.addAttribute("loginMember", loginMember);
-        return "realtor/rMain";
-    }
-	
-	
+	@GetMapping
+	public String mainPage(Model model, HttpSession session) {
+		rMember loginMember = (rMember) session.getAttribute("loginMember");
+		model.addAttribute("loginMember", loginMember);
+		return "realtor/rMain";
+	}
+
 	// 회원가입 페이지로 이동하기
 	@GetMapping("member/join")
 	public String toRealtorJoin() {
 		return "realtor/member/join";
 	}
-	
+
+	// 매물 목록 페이지로 이동하기
+	@GetMapping("list")
+	public String toList(Model model, HttpSession session) {
+		rMember loginMember = (rMember) session.getAttribute("loginMember");
+		model.addAttribute("loginMember", loginMember);
+		return "realtor/list/list";
+	}
+
 	// 로그인 페이지로 이동하기
 	@GetMapping("member/login")
 	public String getLogin() {
 		return "realtor/member/login";
 	}
-	
+
 	// 로그아웃 실행하기
 	@GetMapping("member/logout")
 	public String logout(HttpSession session) {
 		session.removeAttribute("loginMember");
 		return "redirect:/rMain";
 	}
-	
-	
-	// 매물 목록 페이지로 이동하기
-	@GetMapping("list")
-	public String toList() {
-		return "realtor/list/list";
-	}
-	
+
 }
