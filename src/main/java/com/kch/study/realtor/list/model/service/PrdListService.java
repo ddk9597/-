8d92e72e86f69345f572f77df30fc598351ed727
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-08-04 23:54:08 +0900

    no message

---
+++ src/main/java/com/kch/study/realtor/list/model/service/PrdListService.java
@@ -0,0 +1,5 @@
+package com.kch.study.realtor.list.model.service;
+
+public interface PrdListService {
+
+}
---
+++ src/main/java/com/kch/study/realtor/list/model/service/PrdSerivceImpl.java
@@ -0,0 +1,8 @@
+package com.kch.study.realtor.list.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrdSerivceImpl implements PrdListService{
+
+}
