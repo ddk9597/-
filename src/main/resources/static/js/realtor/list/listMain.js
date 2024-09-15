0169af419e2be7ab05a5c9d99957f148d39cae22
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-09-16 05:23:29 +0900

    매물 화살표 대신 원으로 표시

--- src/main/resources/static/js/realtor/list/listMain.js
+++ src/main/resources/static/js/realtor/list/listMain.js
@@ -199,19 +199,21 @@ productInfo.forEach(item => {
 
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
+            // 결과값으로 받은 위치를 마커로 표시
 
-
-            // 결과값으로 받은 위치를 마커로 표시합니다
-            var marker = new kakao.maps.Marker({
-              map: map,
-              position: coords
+            // 지도에 표시할 원 생성
+            var circle = new kakao.maps.Circle({
+              center: coords,  // 원의 중심좌표 입니다 
+              radius: 75, // 미터 단위의 원의 반지름입니다 
+              strokeWeight: 5, // 선의 두께입니다 
+              strokeColor: '#75B8FA', // 선의 색깔입니다
+              strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+              strokeStyle: 'dashed', // 선의 스타일 입니다
+              fillColor: '#CFE7FF', // 채우기 색깔입니다
+              fillOpacity: 0.5  // 채우기 불투명도 입니다   
             });
 
-            // 인포윈도우로 장소에 대한 설명을 표시합니다
-            var infowindow = new kakao.maps.InfoWindow({
-              content: '<div style="width:150px;text-align:center;padding:6px 0;">매물 위치</div>'
-            });
-            infowindow.open(map, marker);
+            circle.setMap(map);
 
             // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
             map.setCenter(coords);
